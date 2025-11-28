3f71d4e24238ab26b2b0fd4c19aaa69c54aa28e2
Armand79 <guillot.armand@gmail.com>
2025-11-28 09:36:47 +0100

    using @Input to get params

--- src/app/components/stagiaire/stagiaire.ts
+++ src/app/components/stagiaire/stagiaire.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -7,20 +7,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './stagiaire.html',
   styleUrl: './stagiaire.css',
 })
-export class StagiaireComponent implements OnInit {
-  nom = "";
-  prenom = "";
-
-  constructor(private route:ActivatedRoute) {}
-  ngOnInit(): void {
-    // Asynchrone
-    // this.route.paramMap.subscribe(params => {
-    //   this.nom = params.get('nom') ?? ''
-    //   this.prenom = params.get('prenom') ?? ''
-    // })
-    // Synchrone
-    this.nom = this.route.snapshot.paramMap.get('nom') ?? ''
-    this.prenom = this.route.snapshot.paramMap.get('prenom') ?? ''
-  }
-
+export class StagiaireComponent {
+  @Input() nom = ""
+  @Input() prenom = ""
 }
