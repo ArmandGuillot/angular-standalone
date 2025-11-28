276e122968ffdf9f5a98bc96e196746b546f2a1f
Armand79 <guillot.armand@gmail.com>
2025-11-28 12:03:13 +0100

    two way binding with ngModel (FormsModule)

--- src/app/components/formulaire-simple/formulaire-simple.ts
+++ src/app/components/formulaire-simple/formulaire-simple.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-simple',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulaire-simple.html',
   styleUrl: './formulaire-simple.css',
 })
 export class FormulaireSimpleComponent {
   nom = 'Doe'
-  updateNom(value: string) {
-    this.nom = value
-  }
+  // updateNom(value: string) {
+  //   this.nom = value
+  // }
   // updateNom(event: Event) {
   //   this.nom = (event.target as HTMLInputElement).value
   // }
