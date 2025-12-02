a239617fa793424bdca6ef471c8d3ebe30934166
Armand79 <guillot.armand@gmail.com>
2025-12-02 13:59:07 +0100

    Exercice Tchat

--- src/app/services/notification.spec.ts
+++ src/app/services/notification.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MessageService } from './message';
+import { NotificationService } from './notification';
 
-describe('Message', () => {
-  let service: MessageService;
+describe('Notification', () => {
+  let service: NotificationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(MessageService);
+    service = TestBed.inject(NotificationService);
   });
 
   it('should be created', () => {
