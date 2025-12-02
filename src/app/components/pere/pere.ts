7079c6cfa9be68810c6b20fe00f2e182012301b4
Armand79 <guillot.armand@gmail.com>
2025-12-02 09:19:13 +0100

    ngAfterViewInit

--- src/app/components/pere/pere.ts
+++ src/app/components/pere/pere.ts
@@ -1,4 +1,4 @@
-import { Component, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, viewChild, ViewChild } from '@angular/core';
 import { FilsComponent } from '../fils/fils';
 
 @Component({
@@ -7,14 +7,26 @@ import { FilsComponent } from '../fils/fils';
   templateUrl: './pere.html',
   styleUrl: './pere.css',
 })
-export class PereComponent {
-  // @ViewChild(FilsComponent) fils!: FilsComponent
-  fils = viewChild(FilsComponent)
+export class PereComponent implements OnInit, AfterViewInit {
+  @ViewChild(FilsComponent) fils!: FilsComponent
+  //Angular 19
+  // fils = viewChild(FilsComponent)
   nom = 'John Wick'
   maVille = 'Marseille'
 
+  //exécutée après l'initialisation du composant Pere
+  ngOnInit(): void {
+    console.log(this.fils);
+  }
+
+  //exécutée après l'initialisation de la vue
+  ngAfterViewInit(): void {
+    console.log(this.fils);
+    
+  }
+
   premierEnfant() {
-    console.log(this.fils());
-    console.log(this.fils()?.ville());
+    console.log(this.fils);
+    console.log(this.fils?.ville());
   }
 }
